c92a22954133b837fabfce9721dfe601c07cdab9
r-ishida0311 <r-ishida@mbs.co.jp>
2024-10-15 10:17:14 +0900

    新規作成の修正

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -1,49 +1,24 @@
 class StoragesController < ApplicationController
-def index
-  @storages = Storage.all
-  @storage = Storage.new
-end
-
-def create
-  @storage = Storage.new(storage_params)
-
-  if @storage.save
-    redirect_to storages_path, notice: 'Storage was successfully created.'
-  else
-    render :new, status: :unprocessable_entity
+  def index
+    @storages = Storage.all
+    @storage = Storage.new
   end
-end
 
-def show
-  @storage = Storage.find(params[:id])
-end
-
-def edit
-  @storage = Storage.find(params[:id])
-end
-
-def update
-  @storage = Storage.find(params[:id])
-
-  if @storage.update(storage_params)
-    redirect_to storages_path, notice: 'Storage was successfully updated.'
-  else
-    render :edit, status: :unprocessable_entity
+  def create
+    @storage = Storage.new(storage_params)
+    if @storage.save
+      redirect_to storages_path, notice: 'Storage was successfully created.'
+    else
+      @storages = Storage.all # Fetch all storages for the index view
+      render :index, status: :unprocessable_entity
+    end
   end
-end
-
-def destroy
-  @storage = Storage.find(params[:id])
-  @storage.destroy
-
-  redirect_to storages_path, notice: 'Storage was successfully destroyed.'
-end
-
-private
-
-def storage_params
-  params.permit(:storage)
-end
 
   # ... other actions ...
+
+  private
+
+  def storage_params
+    params.require(:storage).permit(:storage)
+  end
 end
