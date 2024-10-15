ee109f5d46082af70ce4143a332f63d40fd6b440
r-ishida0311 <r-ishida@mbs.co.jp>
2024-10-16 00:14:31 +0900

    バリデーション修正中

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -1,5 +1,8 @@
 class Shelf < ApplicationRecord
   validates :shelf, presence: true, uniqueness: { scope: :storage_id }
   belongs_to :storage
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "shelf", "updated_at"]
+  end
 end
 
