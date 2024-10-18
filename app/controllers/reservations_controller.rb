b434934a219f1f2f2588e38f26d1cfb963619181
r-ishida0311 <r-ishida@mbs.co.jp>
2024-10-19 00:04:28 +0900

    日付は渡せている

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,9 @@
 class ReservationsController < ApplicationController
   def new
     @reservation = Reservation.new(item_no_id: params[:item_no_id], start_date: params[:date])
-    render partial: 'items/form' # Render the form partial
+    respond_to do |format|
+      format.html { render partial: 'items/form', layout: false } # Render the form partial
+      format.turbo_stream { render turbo_stream: turbo_stream.update('modal', partial: 'items/form') }
+    end
   end
 end
